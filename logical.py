9a5245e6d58fbbb24dba53b5e017e3d6b39ce4a9
youdnchime <02230210.cst@rub.edu.bt>
2024-05-27 17:42:27 +0530

    logical

---
+++ logical.py
@@ -0,0 +1,12 @@
+x = 5
+print(x > 3 and x < 10)
+y = 12
+print(y > 10 and y % 5 == 0)
+x = 5
+print(x < 3 or x > 10)
+y = 12
+print(y < 10 or y % 2 == 0)
+x = 5
+print(not(x > 3 and x < 10))
+y = 12
+print(not(y > 10 and y % 5 == 0))
