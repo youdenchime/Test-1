53e828f936d4a9667fec5c6f25d294e401ffe1e0
youdnchime <02230210.cst@rub.edu.bt>
2024-05-27 17:27:12 +0530

    first program

---
+++ first_program.py
@@ -0,0 +1,14 @@
+print("hi i am rinchen")
+print("Hello, world")
+print("I love programming")
+grade = 70
+if grade>=90:
+    print("Grade: A")
+elif grade>=80:
+    print("Grade: B")
+elif grade>=70:
+    print("Grade: C")
+elif grade>=60:
+    print("Grade: D")
+else:
+    print("Grade: F")
